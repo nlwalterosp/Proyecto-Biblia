cb94c59b157389bbbdcf2b5b6e3c58e6f6fbfb40
MdlMar2027 <marce-caranton@misena.edu.co>
2026-03-02 17:25:10 -0500

    imteraccion 2

--- Assets/Integracion_Tomo1/Escenarios/Escenarios FBX/3.Grandes sueños/Props/Planeta/PlanetaController.cs
+++ Assets/Integracion_Tomo1/Escenarios/Escenarios FBX/3.Grandes sueños/Props/Planeta/PlanetaController.cs
@@ -3,10 +3,23 @@ using UnityEngine;
 public class PlanetaController : MonoBehaviour
 {
     public Animator animatorPlaneta;
+    public NinoEventos nino;
 
     public void SubirPlaneta()
     {
         animatorPlaneta.SetTrigger("Planeta_Subir");
     }
-}
 
+    public void BajarPlaneta()
+    {
+        animatorPlaneta.SetTrigger("Planeta_Bajar");
+    }
+
+    public void AvisarBajadaCompleta()
+    {
+        if (nino != null)
+        {
+            nino.Saludar();
+        }
+    }
+}
